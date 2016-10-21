a71ea1f114f5bce8a8d48fa6313dd8d65f368621
Alex Polishchuk <AlexPolishchukMail@gmail.com>
2016-10-20 21:15:16 -0400

    Initial Upload
    Hello World

---
+++ Basics/test_1.rs
@@ -0,0 +1,14 @@
+fn main()
+{
+	println!("Hello World!");
+	
+	let x: i32 = 5; //declare a constant 32-bit integer x to be 5
+	let mut y = 10;
+	let z = x + y;
+	
+	y = 20;
+	
+	let (a, b) = (1, 2);
+	
+	println!("x = {}", x); //prints the value of x
+}

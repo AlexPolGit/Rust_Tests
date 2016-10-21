773f91c465e1c38c008e71dc13a34e76d3e1b768
Alex Polishchuk <AlexPolishchukMail@gmail.com>
2016-10-20 22:35:11 -0400

    Update 1 of Test 1

--- Basics/test_1.rs
+++ Basics/test_1.rs
@@ -10,5 +10,25 @@ fn main()
 	
 	let (a, b) = (1, 2);
 	
-	println!("x = {}", x); //prints the value of x
+	println!("x = {}, y = {}, z = x + y = {}.", x, y, z); //prints the value of x
+	
+	{
+		let num = 256;
+		let y = -64;
+		
+		println!("Also, num = {} and a different y is {}, num * y = {}", num, y, num * y);
+	}
+	
+	printInt32(x);
+	println!("5 + 1 = {}", addOne(5));
+}
+
+fn printInt32(x: i32)
+{
+	println!("Number printed with a function: {}", x);
+}
+
+fn addOne(x: i32) -> i32
+{
+	x + 1
 }
